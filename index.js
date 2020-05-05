dbfcac7af2a964e5ac125358b1d85e4923fabb16
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-05-05 16:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign({}, object)
+  Object.assign({}, object, [key])
   return Object.assign({}, object)
 }
 
