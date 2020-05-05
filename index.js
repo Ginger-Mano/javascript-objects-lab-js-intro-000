3444d8c911ed813cb9e3390c85043ab71dff9161
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-05-05 16:11:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, { key: value })
-  return Object.assign({}, obj, { [key]: value })
+  object[key] = value;
+  return object
 }
